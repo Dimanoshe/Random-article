bacf50503c6a7d1f07fd398b8a278f11f98451ad
Dimanoshe <dimanoshe@gmail.com>
2021-02-06 00:10:21 +0300

    Add result page

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,19 @@
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.bd'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class Article(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.Text, nullable=False)
+
+    def __repr__(self):
+        return "<Artikle %r>" % self.id
 
 
 @app.route('/', methods=['POST', 'GET'])
@@ -8,13 +21,19 @@ def main():
     index = "No topic selected"
     if request.method == "POST":
         index = request.form['index']
-    return render_template("main.html", index=index)
 
+        text = Article(text=index)
+        print(text)
+        db.session.add(text)
+        db.session.commit()
+
+    return render_template("main.html", index=index)
 
 
 @app.route('/result')
 def result():
-    return render_template("result.html")
+    text = Article.query.all()
+    return render_template("result.html", index=text[-1])
 
 
 if __name__ == '__main__':
